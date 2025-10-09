d4e6b80077fc6dac6746766456057650535c523d
KrSAdal <adal.ac.ramirez@gmail.com>
2025-10-09 17:23:40 -0600

    eliminación logica de clientes y proyectos

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Proyecto extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['nombre', 'cliente_id', 'estado'];
 
     public function cliente()
